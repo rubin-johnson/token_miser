742fd3d6a9f416fb4f124b2caf46851a0f26665f
Rubin Johnson <rubin.lewis.johnson@gmail.com>
2026-03-17 21:13:38 -0700

    feat(BT-004): wire compareCommand to db.InitDB and report.Compare with io.Writer support

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,45 +3,71 @@ package cli
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/rubin-johnson/token_miser/internal/db"
+	"github.com/rubin-johnson/token_miser/internal/report"
 	"github.com/rubin-johnson/token_miser/internal/task"
 )
 
 // Dispatch routes commands to their handlers
-func Dispatch(command string, args []string) error {
+func Dispatch(command string, args []string, out io.Writer) error {
 	switch command {
 	case "run":
-		return runCommand(args)
+		return runCommand(args, out)
 	case "compare":
-		return compareCommand(args)
+		return compareCommand(args, out)
 	case "history":
-		return historyCommand(args)
+		return historyCommand(args, out)
 	case "tasks":
-		return tasksCommand(args)
+		return tasksCommand(args, out)
 	default:
 		return fmt.Errorf("unknown command: %s", command)
 	}
 }
 
-func runCommand(args []string) error {
-	fmt.Println("not implemented")
+func runCommand(args []string, out io.Writer) error {
+	fmt.Fprintln(out, "not implemented")
 	return fmt.Errorf("not implemented")
 }
 
-func compareCommand(args []string) error {
-	fmt.Println("not implemented")
+func compareCommand(args []string, out io.Writer) error {
+	fs := flag.NewFlagSet("compare", flag.ContinueOnError)
+	taskID := fs.String("task", "", "task ID to compare")
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("get home dir: %w", err)
+	}
+	dbPath := filepath.Join(home, ".token_miser", "results.db")
+
+	database, err := db.InitDB(dbPath)
+	if err != nil {
+		return fmt.Errorf("init db: %w", err)
+	}
+	defer database.Close()
+
+	result, err := report.Compare(*taskID, database.Conn())
+	if err != nil {
+		return fmt.Errorf("compare: %w", err)
+	}
+
+	fmt.Fprint(out, result)
+	return nil
+}
+
+func historyCommand(args []string, out io.Writer) error {
+	fmt.Fprintln(out, "not implemented")
 	return fmt.Errorf("not implemented")
 }
 
-func historyCommand(args []string) error {
-	fmt.Println("not implemented")
-	return fmt.Errorf("not implemented")
-}
-
-func tasksCommand(args []string) error {
+func tasksCommand(args []string, out io.Writer) error {
 	fs := flag.NewFlagSet("tasks", flag.ContinueOnError)
 	dir := fs.String("dir", "tasks", "directory containing task YAML files")
 	if err := fs.Parse(args); err != nil {
@@ -63,7 +89,7 @@ func tasksCommand(args []string) error {
 			fmt.Fprintf(os.Stderr, "skip %s: %v\n", e.Name(), err)
 			continue
 		}
-		fmt.Printf("%-20s %s\n", t.ID, t.Name)
+		fmt.Fprintf(out, "%-20s %s\n", t.ID, t.Name)
 	}
 	return nil
 }
